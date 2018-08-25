07f7c9e9ef5d4186cce8200cff935b44021f22bd
katy@karstaway.com <katy@karstaway.com>
2018-08-25 11:46:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?( number )
+
+    result = true
+
+    for i in 2..number - 1
+
+        if number % i == 0
+            result = false
+            break
+        end
+    end
+
+    return result
+end
+
+number = 13
+
+puts "The number #{number} is primes? #{isPrime(number)}"
