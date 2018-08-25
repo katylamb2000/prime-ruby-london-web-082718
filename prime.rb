7112568fcd181272240d238ccc9104c5e9a09431
katy@karstaway.com <katy@karstaway.com>
2018-08-25 11:53:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?( number )
+
+    if number > 0
+      true
+end
+    for i in 2..number - 1
+
+        if number % i == 0 || if number <0 
+            result = false
+            break
+        end
+  
+
+   result
+end
